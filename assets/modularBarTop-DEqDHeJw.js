26105d31bf0243d47e6da03b25c2f3839ba9456a
hetesaus <mhs@live.nl>
2026-03-31 17:06:14 +0200

    Fix for asset loading

---
+++ assets/modularBarTop-DEqDHeJw.js
@@ -0,0 +1,5 @@
+const e="/portfolio/",o={id:"modularBarTop",overviewTags:["Build"],overviewImage:`${e}/assets/overview/modularBarTop.jpg`,year:2018,title:"Modular Bartop Arcade",description:`A custom-built bartop arcade machine running on EmulationStation, capable of emulating systems up to the Nintendo 64 on a Raspberry Pi 3. 
+
+ The cabinet is designed to be fully modular: the control panel, marquee, and monitor can all be removed or replaced. All components are securely mounted using bolts, making it easy to disassemble and upgrade. 
+
+ I designed this system with long-term use in mind, allowing me to continuously improve and modify the machine over time.`,cover:`${e}/assets/projects/modularBarTop/cover.jpg`,images:[`${e}/assets/projects/modularBarTop/screen_01.jpg`,`${e}/assets/projects/modularBarTop/screen_02.jpg`,`${e}/assets/projects/modularBarTop/screen_03.jpg`,`${e}/assets/projects/modularBarTop/screen_04.jpg`],tags:["Arcade","Bartop","Emulation","MDF"]};export{o as default};
